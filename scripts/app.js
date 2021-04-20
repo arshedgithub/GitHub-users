f3c2847bb76f8996de77d60750f5e968983d8efb
arshedgithub <arshedahmedmail@gmail.com>
2021-04-20 23:35:42 +0530

    get user info

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,25 @@
-const getAccount = () => {};
+const form = document.querySelector("form");
+const search = document.querySelector(".search");
+
+const forecast = new Forecast();
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const username = form.search.value.trim();
+  form.reset();
+
+  forecast.getUserInfo(username)
+    .then((data, reposData) => updateUI(data, reposData))
+    .catch((err) => console.log(err));
+});
+
 
 updateUI = (data, reposData) => {
   const { name, url, avatar_url: image } = data.data;
   console.log(name, url, image);
   console.log(data.reposData);
 };
+
+
+
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,18 +1,15 @@
-const username = "arshedgithub";
+class Forecast {
+  constructor() {
+    this.userURI = 'https://api.github.com/users/';
+  }
+  async getUserInfo(username) {
+    const user = `${username}`
+    const response = await fetch(this.userURI + user);
+    const data = await response.json();
 
-const getUser = async (username) => {
-  const userURI = `https://api.github.com/users/${username}`;
-  const repos = "/repos";
+    const reposResponse = await fetch(this.userURI + user + '/repos');
+    const reposData = await reposResponse.json();
 
-  const response = await fetch(userURI);
-  const data = await response.json();
-
-  const reposResponse = await fetch(userURI + repos);
-  const reposData = await reposResponse.json();
-
-  return { data, reposData };
-};
-
-getUser("arshedgithub")
-  .then((data, reposData) => updateUI(data, reposData))
-  .catch((err) => console.log(err));
+    return { data, reposData };
+  };
+}
