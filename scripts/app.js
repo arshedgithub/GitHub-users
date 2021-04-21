e0c031c729f22bf13ad4c385a9c473ef669ee9ef
arshedgithub <arshedahmedmail@gmail.com>
2021-04-21 08:35:54 +0530

    add repos list

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@ const search = document.querySelector(".search");
 const content = document.querySelector(".content");
 const account = document.querySelector(".accountInfo");
 const userInfo = document.querySelector(".userInfo");
-
+const repos = document.querySelector(".repos");
+const repoList = document.querySelector(".repoList");
 
 const forecast = new Forecast();
 
@@ -23,17 +24,51 @@ form.addEventListener('submit', e => {
 
 
 updateUI = (data, reposData) => {
-  const { name, login, html_url: url, avatar_url: image } = data.data;
+  const { name, login, html_url, avatar_url } = data.data;
 
   userInfo.innerHTML = `
-          <img class="rounded-circle" src="${image}" alt="profile_picture" />
-          <h3>${name}</h3>
-          <h6><a href="${url}" target="_blank">${login}</a></h6>
+    <img class="rounded-circle" style="height:200px;" src="${avatar_url}" alt="profile_picture" />
+    <h3 class="my-3">${name}</h3>
+    <h6><a href="${html_url}" target="_blank">${login}</a></h6>
+    `;
+
+  const arrRight = `<i class="fas fa-angle-right"></i>`;
+  const arrDown = `<i class="fas fa-angle-down"></i>`;
+  let arrow = arrRight;
+
+  repos.innerHTML = `
+  <h5 class="py-4" style="cursor: pointer;">${arrow}&nbsp;&nbsp;Repositories &nbsp;&nbsp;${data.reposData.length}</h5>
   `;
-  console.log(data.reposData);
+
+  repos.addEventListener('click', e => {
+    const { target } = e;
+    if (target.parentElement == repos || target == repos) {
+
+      if (arrow == arrRight) {
+        arrow = arrDown;
+        repos.innerHTML = `
+        <h5 class="py-4" style="cursor: pointer;">${arrow}&nbsp;&nbsp; Repositories &nbsp;&nbsp;${data.reposData.length}</h5>
+        `;
+        data.reposData.map(c => {
+          repos.innerHTML += `<li><a href="${c.html_url}" target="_blank">${c.name}</a></li>`;
+        });
+        return;
+
+      } else {
+        arrow = arrRight;
+        repos.innerHTML = `
+        <h5 class="py-4" style="cursor: pointer;">${arrow}&nbsp;&nbsp; Repositories &nbsp;&nbsp;${data.reposData.length}</h5>
+        `;
+        return;
+      }
+
+    }
+  });
+
 };
 
 const goToHome = () => {
+  arrow = '';
   account.classList.add('hidden');
   content.classList.remove('hidden');
 }
