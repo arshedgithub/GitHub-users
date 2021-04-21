28da44862a6375106c4202f6bd0a25c56c34b2e6
arshedgithub <arshedahmedmail@gmail.com>
2021-04-21 05:45:46 +0530

    add pagination

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 const form = document.querySelector("form");
 const search = document.querySelector(".search");
+const content = document.querySelector(".content");
+const account = document.querySelector(".accountInfo");
+const userInfo = document.querySelector(".userInfo");
+
 
 const forecast = new Forecast();
 
@@ -9,6 +13,9 @@ form.addEventListener('submit', e => {
   const username = form.search.value.trim();
   form.reset();
 
+  content.classList.add('hidden');
+  account.classList.remove('hidden');
+
   forecast.getUserInfo(username)
     .then((data, reposData) => updateUI(data, reposData))
     .catch((err) => console.log(err));
@@ -16,10 +23,20 @@ form.addEventListener('submit', e => {
 
 
 updateUI = (data, reposData) => {
-  const { name, url, avatar_url: image } = data.data;
-  console.log(name, url, image);
+  const { name, login, html_url: url, avatar_url: image } = data.data;
+
+  userInfo.innerHTML = `
+          <img class="rounded-circle" src="${image}" alt="profile_picture" />
+          <h3>${name}</h3>
+          <h6><a href="${url}" target="_blank">${login}</a></h6>
+  `;
   console.log(data.reposData);
 };
+
+const goToHome = () => {
+  account.classList.add('hidden');
+  content.classList.remove('hidden');
+}
 
 
 
